aa0dc00f1530252d694eacd0feb3c0b1f7323475
Isma1Huz <ismael.hussein@student.moringaschool.com>
2023-06-18 16:58:56 +0300

    Done with assignments

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+// //Write your solution in this file!
+const employee = {
+    name: "Ismael",
+    streetAddress: "Ngong"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return Object.assign({}, employee, {[key]: value });
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key]= value;
+    return employee
+}
+function deleteFromEmployeeByKey(employee, key){
+    let newobj = { ...employee };
+    delete newobj[key];
+    return newobj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
+
+
+
+
+
+
+
+
+
+
